9d20059fae8f9381378f11a705e918e2e5713432
nazanin_ys <nazaninyousefian@yahoo.com>
2021-05-18 01:29:17 +0430

    errorItemMessage changed

--- src/main/compileError/typeErrors/ListElementsTypeNotMatch.java
+++ src/main/compileError/typeErrors/ListElementsTypeNotMatch.java
@@ -4,6 +4,6 @@ import main.compileError.CompileError;
 
 public class ListElementsTypeNotMatch extends CompileError {
     public ListElementsTypeNotMatch(int line) {
-        super(line, "List elements type do not match");
+        super(line, "Elements of the list have different types");
     }
 }
